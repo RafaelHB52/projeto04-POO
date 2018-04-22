8d2436d9e8b532ad595c12768272240b4763b7a8
10joaopaulo <jp.1998.jpd@icloud.com>
2018-04-22 12:22:06 -0300

    Dados Fornecedor

---
+++ src/java/br/com/fatecpg/dados/Fornecedor.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.dados;
+
+/**
+ *
+ * @author JoãoPaulo
+ */
+public class Fornecedor {
+    private String nome;
+    private String razao;
+    private String cnpj;
+    private String email;
+    private String tel;
+    private String end;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazao() {
+        return razao;
+    }
+
+    public void setRazao(String razao) {
+        this.razao = razao;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+}
