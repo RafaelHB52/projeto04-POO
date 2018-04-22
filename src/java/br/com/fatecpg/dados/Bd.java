ab9e91dd36fa485ed8a3a2ca15cb02412fdb3da5
Rafael <rafael_barbosa152@hotmail.com>
2018-04-22 12:40:34 -0300

    Ajuste nos links da navbar

--- src/java/br/com/fatecpg/dados/Bd.java
+++ src/java/br/com/fatecpg/dados/Bd.java
@@ -2,7 +2,7 @@ package br.com.fatecpg.dados;
 import java.util.ArrayList;
 
 public class Bd {
-    private static ArrayList<Cliente> cliente = new ArrayList<>();
+ private static ArrayList<Cliente> cliente = new ArrayList<>();
  private static ArrayList<Fornecedor> fornecedor = new ArrayList<>();
  
  
